a26e04dea6fef58c087671265fa6c76a31016c30
antodarr <antonio.darrigoct@gmail.com>
2026-07-15 10:55:58 +0200

    build: switch to hybrid output with the Vercel serverless adapter

--- butto-srl/astro.config.mjs
+++ butto-srl/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://butto-srl.it',
+  output: 'hybrid',
+  adapter: vercel(),
 });
